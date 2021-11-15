b3d7d9b6615f95cdf8f036a4ed887af8f5ee1c66
yujeong <kyuj1102@gmail.com>
2021-11-16 02:25:03 +0900

    10872

---
+++ 10872.py
@@ -0,0 +1,14 @@
+import sys 
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+
+def fac(num):
+    if(num==0):
+        return 1
+    
+    else:
+        return num * fac(num-1)
+    
+
+print(fac(n))
