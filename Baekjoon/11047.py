10cd4c500fa67ed9b0e3b1d79ade78ba8bb2bc8f
yujeong-kwon <94073724+yujeong-kwon@users.noreply.github.com>
2021-11-19 15:56:04 +0900

    Create 11047.py

---
+++ Baekjoon/11047.py
@@ -0,0 +1,23 @@
+import sys 
+input = sys.stdin.readline
+
+n, k = map(int,input().rstrip().split())
+money=[]
+for _ in range(n):
+    num = int(input())
+    money.append(num)
+
+cnt = 0
+
+
+for i in range(len(money), 0, -1):
+    cnt += k // money[i-1]
+    k = k % money[i-1]
+    
+print(cnt)
+
+
+#  if K == 0 :
+#    break
+#  count += (K // i)
+#  K %= i
