ca54360380afb7219435c0244cb4b2a6d2a9cd84
Kyujeong <kyuj1102@gmail.com>
2022-02-07 03:19:58 +0900

    백준 1단계_입출력과 사칙연산

---
+++ Baekjoon/1_입출력과 사칙연산/10926_??!.py
@@ -0,0 +1,5 @@
+import sys
+input = sys.stdin.readline
+
+id = input().rstrip()
+print(id + "??!")
---
+++ Baekjoon/1_입출력과 사칙연산/18108_1998년생인 내가 태국에서는 2541년생?!.py
@@ -0,0 +1,6 @@
+import sys
+input = sys.stdin.readline
+
+num = int(input().rstrip())
+res = num - 543
+print(res)
