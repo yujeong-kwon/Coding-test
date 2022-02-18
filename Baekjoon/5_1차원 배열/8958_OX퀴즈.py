a850212279b07ef833c18a643cf0cd9b93a006eb
Kyujeong <kyuj1102@gmail.com>
2022-02-18 18:47:07 +0900

    0218_upload1

--- Baekjoon/5_1차원 배열/8958_OX퀴즈.py
+++ Baekjoon/5_1차원 배열/8958_OX퀴즈.py
@@ -1,2 +1,15 @@
 import sys
 input = sys.stdin.readline
+
+n = int(input().rstrip())
+for i in range(n):
+    score = list(input().rstrip())
+    sum = 0
+    cnt = 1
+    for j in score:
+        if j == "O":
+            sum += cnt
+            cnt += 1
+        elif j == "X":
+            cnt = 1
+    print(sum)
