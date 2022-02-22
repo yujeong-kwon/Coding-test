8e0558edcdbd4c897feea5fdd5f2d693895b754c
Kyujeong <kyuj1102@gmail.com>
2022-02-22 22:37:37 +0900

    0222_upload1

--- Baekjoon/7_문자열/1316_그룹 단어 체커.py
+++ Baekjoon/7_문자열/1316_그룹 단어 체커.py
@@ -5,10 +5,14 @@ n = int(input().rstrip())
 cnt = 0 
 for _ in range(n):
     word = input().rstrip()
-
+    error = 0
     for i in range(len(word)-1):
         if word[i] != word[i+1]:
             new = word[i+1:]
-            if new.count(word[i]) == 0:
-                cnt += 1
+            if new.count(word[i]) > 0:
+                error += 1
+    if error == 0:
+        cnt += 1
 print(cnt)
+
+#복습
