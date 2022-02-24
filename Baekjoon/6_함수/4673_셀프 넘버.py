24a170b984c767e9ab7bdbaf15e4ab3c0bf2fc5e
Kyujeong <kyuj1102@gmail.com>
2022-02-25 01:43:24 +0900

    0224_modify1

--- Baekjoon/6_함수/4673_셀프 넘버.py
+++ Baekjoon/6_함수/4673_셀프 넘버.py
@@ -1,3 +1,11 @@
-def solve():
-    for i in range(1, 10000):
-        
+natural_num = set(range(1, 10001))
+generated_num = set() #생성자들
+
+for i in range(1, 10001):
+    for j in str(i):
+        i += int(j)
+    generated_num.add(i)
+
+self_num = sorted(natural_num - generated_num)
+for i in self_num:
+    print(i)
